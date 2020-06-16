a622ed393539e4c6e6de7914812a8013e340264d
Pieter <pdutoit@fullfacing.com>
2020-06-16 21:08:07 +0200

    set up react auth seed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+axios.interceptors.request.use(
+  config => {
+    const { origin } = new URL(config.url)
+    const allowedOrigins = [apiUrl]
+    const token = localStorage.getItem('token')
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
 
 function App() {
+
+  const storedJwt = localStorage.getItem('token')
+  const storedUser = JSON.parse(localStorage.getItem('user'))
+
+  const [authToken, setAuthToken] = useState(storedJwt)
+  const [user, setUser] = useState(storedUser)
+
+  function clearUser() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    setAuthToken(null)
+    setUser(null)
+  }
+
+  const handleSignIn = async () => {
+    try {
+      const { data } = await axios.post(`${apiUrl}/user/login`, {
+        email: 'elliot@test.com',
+        password: 'admin'
+      })
+      const { accessToken, user } = data 
+
+      localStorage.setItem('token', accessToken)
+      localStorage.setItem('user', JSON.stringify(user))
+
+      setAuthToken(accessToken)
+      setUser(user)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleLogOut = async () => {
+    try {
+      await axios.post(`${apiUrl}/user/logout`)
+      clearUser()
+      
+    } catch (error) {
+      console.error(error)
+      if (error.response.status === 401) clearUser()
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>
+        {!!authToken && user && `Logged in as: ${user.name}`}
+      </h4>
+      <button onClick={user ? handleLogOut : handleSignIn}>
+        {!!authToken ? 'Log out' : 'Sign in'}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
